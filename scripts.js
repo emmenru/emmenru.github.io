6bc4cea9b100bdf7c339dc970ba89e08a7063397
emmenru <emmenru@gmail.com>
2025-04-14 18:48:23 +0200

    Fix accessibility bugs for screen readers

--- scripts.js
+++ scripts.js
@@ -29,7 +29,9 @@ function myFunction() {
     var x = document.getElementById("navDemo");
     if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
+        x.setAttribute("aria-hidden", "false"); // Make visible to screen readers when opened
     } else { 
         x.className = x.className.replace(" w3-show", "");
+        x.setAttribute("aria-hidden", "true"); // Hide from screen readers when closed
     }
 }
